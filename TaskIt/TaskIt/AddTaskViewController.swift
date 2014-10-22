361c25a4958a091bdc8564636ec845f413d70ced
Stephane Liu <sliu@sjliu.com>
2014-10-21 21:26:20 -1000

    TaskIt Coredata - section 4

--- TaskIt/TaskIt/AddTaskViewController.swift
+++ TaskIt/TaskIt/AddTaskViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class AddTaskViewController: UIViewController {
   
@@ -33,8 +34,28 @@ class AddTaskViewController: UIViewController {
   }
   
   @IBAction func addTaskButtonPressed(sender: UIButton) {
-    var task = TaskModel(task: taskTextField.text, subTask: subtaskTextField.text, date: dueDatePicker.date, completed: false)
-    mainVC.baseArray[0].append(task)
+    // UIApplication - only one instance per application and is a global context object
+    let appDelegate          = (UIApplication.sharedApplication().delegate as AppDelegate)
+    let managedObjectContext = appDelegate.managedObjectContext
+    let entityDescription    = NSEntityDescription.entityForName("TaskModel", inManagedObjectContext: managedObjectContext!)
+    let task                 = TaskModel(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
+    
+    task.task      = taskTextField.text
+    task.subtask   = subtaskTextField.text
+    task.date      = dueDatePicker.date
+    task.completed = false
+    
+    appDelegate.saveContext()
+    
+    var request         = NSFetchRequest(entityName: "TaskModel")
+    var error:NSError?  = nil
+    // & is an address location - a memory optimization feature
+    var results:NSArray = managedObjectContext!.executeFetchRequest(request, error: &error)!
+    
+    for res in results {
+      println(res)
+    }
+    
     self.dismissViewControllerAnimated(true, completion: nil)
   }
 }
