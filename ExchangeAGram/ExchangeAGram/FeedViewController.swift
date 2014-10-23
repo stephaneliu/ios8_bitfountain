439f901308958d4fa1bcb7477aee64b4bf2f331c
Stephane Liu <sliu@sjliu.com>
2014-10-22 22:21:29 -1000

    ExchangeAGram - Section 3

---
+++ ExchangeAGram/ExchangeAGram/FeedViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FeedViewController.swift
+//  ExchangeAGram
+//
+//  Created by Stephane Liu on 10/22/14.
+//  Copyright (c) 2014 Simplified Technologies, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
