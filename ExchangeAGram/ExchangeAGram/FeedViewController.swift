1fabb68ae387344a02b52be02e451cd8ec4e1c71
Stephane Liu <sliu@sjliu.com>
2014-10-23 20:03:09 -1000

    ExchangeAGram - Section 4

--- ExchangeAGram/ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/ExchangeAGram/FeedViewController.swift
@@ -8,29 +8,42 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
-    @IBOutlet weak var collectionView: UICollectionView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
+  // UICollectionViewDataSource
+  func numberofSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    return UICollectionViewCell()
+  }
+  
 }
